ee63eb0dd77cb11c1d492e7a309c04c2f0c1db47
Juan Felipe Bautista <juanfelipebautista12@gmail.com>
2025-11-11 13:03:57 -0500

    elimine el archivo

--- clase.py
+++
@@ -1,9 +0,0 @@
-numeros = [2,4,5,6,7,8,]
-animales= ["burro", "peña", "caiman","perro","gato"]
-
-def primerafuncion (cosas):
-    first= cosas[0]
-    last= cosas[3]
-    return first,last 
-animal1= primerafuncion(animales)
-numero2= primerafuncion(numeros) 
